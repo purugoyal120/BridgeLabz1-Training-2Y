0513849509862099635f06d6948500dc810f3ed0
Puru Goyal <puru.goyal_cs.aiml24@gla.ac.in>
2025-11-13 16:30:31 +0530

    Delete Packages directory

--- Packages/Company Analytics (Sub-Packages)/com/company/analytics/sales/SalesReport.java
+++
@@ -1,7 +0,0 @@
-package com.company.analytics.sales;
-
-public class SalesReport {
-    public void showSales() {
-        System.out.println("Region-wise sales data...");
-    }
-}
--- Packages/Modular Programming (Java 9+)/app/MainApp.java
+++
@@ -1,7 +0,0 @@
-import college.student.Student;
-
-public class MainApp {
-    public static void main(String[] args) {
-        new Student().show();
-    }
-}
