8cf9c2ba1fa3fa118663d81a42e9da0426aab2dc
Puru Goyal <puru.goyal_cs.aiml24@gla.ac.in>
2025-11-13 16:36:46 +0530

    Delete Packages directory

--- Packages/Company Analytics (Sub-Packages)/MainApp.java
+++
@@ -1,9 +0,0 @@
-import com.company.analytics.sales.SalesReport;
-import com.company.analytics.hr.EmployeeReport;
-
-public class MainApp {
-    public static void main(String[] args) {
-        new SalesReport().showSales();
-        new EmployeeReport().showPerformance();
-    }
-}
--- Packages/Company Analytics (Sub-Packages)/com/company/analytics/hr/EmployeeReport.java
+++
@@ -1,7 +0,0 @@
-package com.company.analytics.hr;
-
-public class EmployeeReport {
-    public void showPerformance() {
-        System.out.println("Employee performance report...");
-    }
-}
