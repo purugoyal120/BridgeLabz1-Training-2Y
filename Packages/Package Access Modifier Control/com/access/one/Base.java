039386697b7c7d87af24bfcfed60c61d3e13ad12
Puru Goyal <puru.goyal_cs.aiml24@gla.ac.in>
2026-02-09 14:06:01 +0530

    Delete Packages directory

--- Packages/Package Access Modifier Control/com/access/one/Base.java
+++
@@ -1,8 +0,0 @@
-package com.access.one;
-
-public class Base {
-    public void pubMethod() { System.out.println("Public method"); }
-    protected void proMethod() { System.out.println("Protected method"); }
-    void defMethod() { System.out.println("Default method"); }
-    private void priMethod() { System.out.println("Private method"); }
-}
